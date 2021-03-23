f6705f6b85c1cd16068ab0dce159d88c5ef5c6a8
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-03-23 15:34:36 -0700

    Add resource group detection logic

--- 0-set-cosmosdb-environment.sh
+++ 0-set-cosmosdb-environment.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-RESOURCE_GROUP=$(az group list --query "[?contains(name, 'azure-lab')].name" --output tsv)
+
+if [ -n "${RESOURCE_GROUP}" ];
+then
+    RESOURCE_GROUP=$(az group list --query "[?contains(name, 'azure-lab')].name" --output tsv)
+fi
 
 if [ -z "${RESOURCE_GROUP}" ];
 then
-    echo "Please manually set the RESOURCE_GROUP environment variable and rerun this script."
+    echo
+    echo "ERROR: The resource group could not be set automatically."
+    echo "       Please manually set the RESOURCE_GROUP environment"
+    echo "       variable and rerun this script."
     echo
     echo "For example:"
     echo
@@ -16,14 +23,22 @@ else
     DB_USERNAME="${RESOURCE_GROUP}"
     DB_PASSWORD=$(az cosmosdb keys list --resource-group "${RESOURCE_GROUP}" --name "${RESOURCE_GROUP}" --query primaryMasterKey --output tsv)
 
-    export DB_HOSTNAME
-    export DB_USERNAME
-    export DB_PASSWORD
+    if [ -z "${DB_HOSTNAME}" ] || [ -z "${DB_USERNAME}" ] || [ -z "${DB_PASSWORD}" ];
+    then
+        echo "ERROR: All of the environment variables cloud not be set."
+        echo "       Please confirm that the Cassandra Cosmos DB has the"
+        echo "       exact same name as the resource group ${RESOURCE_GROUP}."
+        echo
+    else
+        export DB_HOSTNAME
+        export DB_USERNAME
+        export DB_PASSWORD
 
-    echo "Environment set successfully!"
-    echo
-    echo "    DB_HOSTNAME = ${DB_HOSTNAME}"
-    echo "    DB_USERNAME = ${DB_USERNAME}"
-    echo "    DB_PASSWORD = ${DB_PASSWORD}"
-    echo
+        echo "Environment set successfully!"
+        echo
+        echo "    DB_HOSTNAME = ${DB_HOSTNAME}"
+        echo "    DB_USERNAME = ${DB_USERNAME}"
+        echo "    DB_PASSWORD = ${DB_PASSWORD}"
+        echo
+    fi
 fi
