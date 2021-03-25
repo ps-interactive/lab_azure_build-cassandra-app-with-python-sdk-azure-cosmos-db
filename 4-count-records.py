ca483750df4e28f7521467f0643afc5953a39d26
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-03-24 20:17:21 -0700

    Add count records

---
+++ 4-count-records.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+'''Count records by gender'''
+from cassandra.auth import PlainTextAuthProvider
+from prettytable import PrettyTable
+from cassandra.cluster import Cluster
+from ssl import PROTOCOL_TLSv1_2, SSLContext, CERT_NONE
+import config as cfg
+
+ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+ssl_context.verify_mode = CERT_NONE
+auth_provider = PlainTextAuthProvider(username=cfg.config['username'], password=cfg.config['password'])
+cluster = Cluster([cfg.config['contactPoint']], port=cfg.config['port'], auth_provider=auth_provider, ssl_context=ssl_context)
+session = cluster.connect()
+
+report = {}
+sexes = ['M', 'F']
+
+print("Counting records ...")
+
+total = session.execute("SELECT COUNT(*) FROM customers.records").one()
+t = PrettyTable(['Total'])
+t.add_row([total.system_count])
+print(t)
